e524210140e9950b2944adeec9751f017ed4ac37
Odiase Samson <odiase.samson@gmail.com>
2022-05-10 12:28:04 +0100

    drop db before seeding users and product

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -7,7 +7,7 @@ import { isAdmin, isAuth, isSellerOrAdmin } from '../utils.js';
 const productRouter = express.Router();
 
 productRouter.get('/seed', expressAsyncHandler(async (req, res) => {
-    // await Product.remove({});
+    await Product.remove({});
     const createdProducts = await Product.insertMany(data.products);
     res.send({ createdProducts });
 }));
